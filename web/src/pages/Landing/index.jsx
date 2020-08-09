73500e9cc8301ef7259de5aefe590d7ba5e1bbfe
giandalpont <giandalpont@gmail.com>
2020-08-09 02:07:05 -0300

    refactor: add link route in Landing page

--- web/src/pages/Landing/index.jsx
+++ web/src/pages/Landing/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import logoImg from '../../assets/logo.svg'
 import landingImg from '../../assets/landing.svg'
@@ -22,14 +23,14 @@ function Landing () {
           alt="Plataforma de estudos"
         />
         <div className="buttons-container">
-          <a href="/study" className="study" >
+          <Link to="/study" className="study" >
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
-          <a href="/give-classes" className="give-classes">
+          </Link>
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas"/> 
             Dar aulas
-          </a>
+          </Link>
         </div>
         <span className="total-connections">
           Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração"/>
