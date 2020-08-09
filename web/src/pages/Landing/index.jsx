d44ffbdfa840719c12d2714a6d8d36b0569fc5a7
giandalpont <giandalpont@gmail.com>
2020-08-09 00:13:39 -0300

    refactor: landing page style

--- web/src/pages/Landing/index.jsx
+++ web/src/pages/Landing/index.jsx
@@ -1,9 +1,40 @@
 import React from 'react'
 
+import logoImg from '../../assets/logo.svg'
+import landingImg from '../../assets/landing.svg'
+import studyIcon from '../../assets/icon/study.svg'
+import giveClassesIcon from '../../assets/icon/give-classes.svg'
+import purpleHeartIcon from '../../assets/icon/purple-heart.svg'
+
+import './styles.min.css'
+
 function Landing () {
   return (
-    <div id="page-landding">
-      <h1>Landing</h1>
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <div className="logo-container">
+          <img src={logoImg} alt="Proffy"/>
+          <h2>Sua plataforma de estudos online</h2>
+        </div>
+        <img 
+          className="hero-image" 
+          src={landingImg} 
+          alt="Plataforma de estudos"
+        />
+        <div className="buttons-container">
+          <a href="google.com.br" className="study" >
+            <img src={studyIcon} alt="Estudar"/>
+            Estudar
+          </a>
+          <a href="google.com.br" className="give-classes">
+            <img src={giveClassesIcon} alt="Dar aulas"/> 
+            Dar aulas
+          </a>
+        </div>
+        <span className="total-connections">
+          Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração"/>
+        </span>
+      </div>
     </div>
   )
 }
