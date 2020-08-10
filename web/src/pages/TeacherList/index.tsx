300392cf6f2e7190852b18f76d4d660cc48c2fa6
giandalpont <giandalpont@gmail.com>
2020-08-10 00:58:35 -0300

    feat: structured input and style header in the TeacherList page

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -6,7 +6,22 @@ import './styles.css'
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
-      <PageHeader title="Esses são os Proffy disponíveis." />
+      <PageHeader title="Esses são os Proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subject"/>
+          </div>
+          <div className="input-block">
+            <label htmlFor="week_day">Dia da semana</label>
+            <input type="text" id="week_dey" />
+          </div>
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
     </div>
   )
 }
