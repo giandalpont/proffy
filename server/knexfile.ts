d33939e6e84029545853eb51e32f8965cf9266bb
giandalpont <giandalpont@gmail.com>
2020-08-11 22:41:07 -0300

    refactor: writes the wrong call to the knexfile.ts file

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -5,8 +5,8 @@ module.exports = {
   connection: {
     filename: path.resolve(__dirname, 'src', 'database','database.sqlite'),
   },
-  miagrations: {
+  migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations')
   },
-  useNullAsDefaul: true,
+  useNullAsDefault: true,
 }
