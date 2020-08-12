5a1384186c7a94c70db7e8a5def04b0c991f5034
giandalpont <giandalpont@gmail.com>
2020-08-11 21:45:22 -0300

    feat: start server router

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,11 @@
-console.log('Hello World 2');
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World'});
+})
+
+app.listen(3333);
