f81daa57a99c5e484d8efa21f17010c3b98abb01
giandalpont <giandalpont@gmail.com>
2020-08-13 02:06:31 -0300

    refactor: routes POST /classes handling 400 error with try catch and rollback

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -21,39 +21,47 @@ routes.post('/classes', async (request, response) => {
   //  Ultizando transection para tratamento de erro de inserção, se acasso alguma tabela não iserir os dados coretamente
   const trx = await db.transaction();
   
-  // Inserindo na tabela users e pegando retorno
-  const insertedUserid = await trx('users').insert({
-    name, avatar, whatsapp, bio 
-  });
+  try {
+    // Inserindo na tabela users e pegando retorno
+    const insertedUserid = await trx('users').insert({
+      name, avatar, whatsapp, bio 
+    });
 
-  // pegando id do user inserido
-  const user_id = insertedUserid[0];
+    // pegando id do user inserido
+    const user_id = insertedUserid[0];
 
-  // salvando tabela relacionada e pegando retorno
-  const insertedClassesIds = await trx('classes').insert({
-    subject, cost, user_id
-  })
- 
-  // pegando id da classes inserida
-  const class_id = insertedClassesIds[0];
+    // salvando tabela relacionada e pegando retorno
+    const insertedClassesIds = await trx('classes').insert({
+      subject, cost, user_id
+    })
+  
+    // pegando id da classes inserida
+    const class_id = insertedClassesIds[0];
 
-  // Passa pelo schedule pegando week_day, from, to and class_id
-  const classSchedule = schedule.map((item: ScheduleItem) => {
-    return {
-      class_id,
-      week_day: item.week_day,
-      from: convertHourToMinuts(item.from),
-      to: convertHourToMinuts(item.to),
-    }
-  })
+    // Passa pelo schedule pegando week_day, from, to and class_id
+    const classSchedule = schedule.map((item: ScheduleItem) => {
+      return {
+        class_id,
+        week_day: item.week_day,
+        from: convertHourToMinuts(item.from),
+        to: convertHourToMinuts(item.to),
+      }
+    })
 
-  // Salvando na tabela class_schedule
-  await trx("class_schedule").insert(classSchedule)
+    // Salvando na tabela class_schedule
+    await trx("class_schedule").insert(classSchedule)
 
-  //  vai fazer as alterações no banco de dados
-  await trx.commit();
+    //  vai fazer as alterações no banco de dados
+    await trx.commit();
 
-  return response.send();
+    return response.status(201).send();
+  } catch (err) {
+    await trx.rollback();
+
+    return response.status(400).json({
+      error: 'Unexpect error while creating new class'
+    })
+  }
 })
 
 export default routes;
