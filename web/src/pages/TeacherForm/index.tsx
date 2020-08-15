27061bac1908b524858ba9799167149ae846f07f
giandalpont <giandalpont@gmail.com>
2020-08-15 01:46:58 -0300

    feat: style and structure page in the cadastre TeacherForm

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import PageHeader from '../../components/PageHeader'
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+import Input from '../../components/Input';
 
-import './styles.css'
+import './styles.min.css';
+import warningIcon from '../../assets/icon/warning.svg';
 
 function TeacherForm() {
-    return (
-        <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrível que você quer dar aulas" />
-        </div>
-    )
+  return (
+    <div id="page-teacher-form" className="container">
+      <PageHeader
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+          <Input name="name" label="Nome completo" />
+          <Input name="avater" label="Avatar" />
+          <Input name="whatsapp" label="WhastApp"  />
+        </fieldset>
+        <fieldset>
+          <legend>Sobre a aula</legend>
+          <Input name="subject" label="Matéria" />
+          <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+        <footer>
+          <p>
+            <img src={warningIcon} alt="Áviso importante" />
+            Importatne! <br />
+            Preencha todos os dados
+          </p>
+          <button type="button">
+            Salvar cadastro
+          </button>
+        </footer>
+      </main>
+    </div>
+  )
 }
 
 export default TeacherForm
