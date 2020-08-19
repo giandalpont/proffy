bd676e2af6145723e1e9fb41bd4fd6fb7367780e
giandalpont <giandalpont@gmail.com>
2020-08-19 20:05:25 -0300

    refactor: directed to landing page after teacher registration

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,6 @@
 import React, { useState, FormEvent } from 'react';
+import { useHistory } from 'react-router-dom';
+
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import warningIcon from '../../assets/icon/warning.svg';
@@ -9,6 +11,8 @@ import './styles.min.css';
 import api from '../../services/api';
 
 function TeacherForm() {
+  const history = useHistory();
+
   const [name, setName] = useState('');
   const [avatar, setAvatar] = useState('');
   const [whatsapp, setWhatsapp] = useState('');
@@ -41,6 +45,8 @@ function TeacherForm() {
 
     }).then(() => {
       alert('Cadastro realizado com sucesso!');
+
+      history.push('/');
     }).catch(() => {
       alert('Erro no cadastro!');
     });
