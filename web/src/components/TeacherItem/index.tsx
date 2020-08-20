dce3ebf8e3cfeb8e095e49f0ae514c9d325bc127
giandalpont <giandalpont@gmail.com>
2020-08-19 21:55:39 -0300

    refactor: teacher listing

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,27 +3,41 @@ import React from 'react'
 import whatsappIcon from '../../assets/icon/whatsapp.svg'
 import './styles.css'
 
-function TeacherItem() {
+export interface TeacherProps {
+  id: number;
+  name: string;
+  avatar: string;
+  bio: string;
+  cost: number;
+  subject: string;
+  whastapp: string;
+}
+
+interface TeacherItemProps {
+  teacher: TeacherProps;
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
   return (
     <main>
       <article className="teacher-item">
         <header>
-          <img src="https://avatars0.githubusercontent.com/u/35267440?s=460&u=dbc4b151478e06291f3048d65f9cfc58c9515a3f&v=4" alt="Gian Dal Pont"/>
+          <img src={teacher.avatar} alt={teacher.name}/>
           <div>
-            <strong>Gian Dal Pont</strong>
-            <span>Física</span>
+            <strong>{teacher.name}</strong>
+            <span>{teacher.subject}</span>
           </div>
         </header>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores iure debitis nesciunt enim explicabo reiciendis. Alias, ipsum repudiandae. Aut, ad suscipit. Distinctio accusantium asperiores doloremque numquam obcaecati consequuntur sapiente sint.</p>
+        <p>{teacher.bio}</p>
         <footer>
           <p>
             preço/hora
-            <strong>R$ 80,00</strong>
+            <strong>R$ {teacher.cost}</strong>
           </p> 
-          <button type="button">
+          <a href={`https://wa.me/${teacher.whastapp}`} type="button">
             <img src={whatsappIcon} alt="WhatsApp"/>
             Entrar em contato
-          </button>
+          </a>
         </footer> 
       </article>
     </main>
